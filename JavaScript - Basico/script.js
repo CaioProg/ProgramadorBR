f5cc7cb70aca4b13d02083f38b68d396babdc81e
CaioProg <caioprog18@gmail.com>
2022-01-16 10:31:51 -0300

    Alert e Prompt

--- JavaScript - Basico/script.js
+++ JavaScript - Basico/script.js
@@ -1,48 +1,7 @@
-var idade = 20;
+var numero1 = parseInt(prompt("Digite o primeiro número"))
+var numero2 = parseInt(prompt("Digite o segundo número"))
+var numero3 = parseInt(prompt("Digite o terceiro número"))
 
-var maior20 = idade >= 20;
-var menor30 = idade <= 30;
+var media = (numero1 + numero2 + numero3) / 3 
 
-var entre = maior20 && menor30;
-console.log("&&") 
-
-console.log("Idade:", idade);
-
-console.log("Maior ou igual a vinte ", maior20)
-
-console.log("Menor ou igual a trinta ", menor30)
-
-console.log("Entre 20 e 30", entre)
-
-console.log("----------------------------------")
-
-var idade = 80;
-
-var menor10 = idade <= 10;
-var maior65 = idade >= 65;
-
-var gratuidade = menor10 || maior65;
-console.log("||") 
-
-console.log("Idade:", idade);
-
-console.log("Maior ou igual a 65 ", maior65)
-
-console.log("Menor ou igual a 10 ", menor10)
-
-console.log("Tem direito a gratuidade?", gratuidade)
-
-console.log("----------------------------------")
-console.log("!") 
-
-var idade = 15;
-
-var maior20 = idade >= 20;
-var menor20 = !maior20;
-
-console.log("Idade:", idade);
-
-console.log("Maior que vinte", maior20)
-
-console.log("Menor que vinte", menor20)
-
+alert("A média dos numero informados é: " + media)

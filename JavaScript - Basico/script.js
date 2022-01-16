5fbd52495d0767c99d980e28ecf6a5831ae6b971
CaioProg <caioprog18@gmail.com>
2022-01-16 10:42:28 -0300

    Condicionais - IF

--- JavaScript - Basico/script.js
+++ JavaScript - Basico/script.js
@@ -1,7 +1,10 @@
-var numero1 = parseInt(prompt("Digite o primeiro número"))
-var numero2 = parseInt(prompt("Digite o segundo número"))
-var numero3 = parseInt(prompt("Digite o terceiro número"))
+var idade = 19;
 
-var media = (numero1 + numero2 + numero3) / 3 
-
-alert("A média dos numero informados é: " + media)
+if (idade >= 18 && idade <= 70){
+  console.log("Pode")
+  console.log("Qual o seu pedido?")
+}
+else{
+  console.log("Não pode")
+  console.log("Volde futuramente")
+}

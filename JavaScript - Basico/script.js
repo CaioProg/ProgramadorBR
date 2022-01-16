356bfa6032a3764dfb37c9a322eab906f9c2b28e
CaioProg <caioprog18@gmail.com>
2022-01-16 11:01:10 -0300

    Condicionais - Switch

--- JavaScript - Basico/script.js
+++ JavaScript - Basico/script.js
@@ -1,3 +1,41 @@
-var idade = 18;
+var nota1 = 6.0;
+var nota2 = 8.8;
 
-idade >= 18 ? console.log("Pode") : console.log("Não pode");
+var media = (nota1 + nota2) / 2;
+var conceito = "";
+
+if (media >= 8) {
+
+  conceito = "Ótimo";
+  
+}
+else if( media >= 6.5) {
+
+  conceito = "Bom";
+
+} else {
+
+  conceito = "Regular";
+
+}
+
+console.log(media);
+console.log(conceito);
+
+switch(conceito){
+
+  case "Ótimo":
+    console.log("Parabéns, você é um ótimo aluno!")
+    break;
+  case "Bom":
+    console.log("Parabéns, você é quase perfeito!")
+    break;
+
+  case "Regular":
+    console.log("Estude mais um pouco!")
+    break;
+
+  default:
+    console.log("Houve algum erro ://")
+    break;
+}

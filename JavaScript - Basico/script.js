276a3373d701b50953343a047f6090a9c2120ec4
CaioProg <caioprog18@gmail.com>
2022-01-17 19:32:08 -0300

    Juntando tudo até aqui

--- JavaScript - Basico/script.js
+++ JavaScript - Basico/script.js
@@ -1,6 +1,27 @@
-var media = function (n1, n2) {
+var nomes = ["Caio", "João", "Astrogildo"];
+var notasA = [7.0, 6.5, 9.5];
+var notasB = [8.0, 7.0, 8.5];
 
-  return (n1 + n2) / 2
+function media(n1,n2) {
+  return (n1 + n2) / 2;
 }
-var m = media(5,6);
-console.log(m);
+
+function passou(media) {
+
+  if(media > 7){
+    return "Aprovado";
+  }else{
+    return "Reprovado";
+  }
+}
+
+for (var index in nomes) {
+
+  var nota1 = notasA[index];
+  var nota2 = notasB[index];
+
+  var m = media(nota1, nota2);
+
+  console.log(nomes[index] + " - " + nota1 + " - " + nota2 + " - " + m + " - " + passou(m));                                 
+  
+}

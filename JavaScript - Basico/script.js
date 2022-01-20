594cc63d9cf1d6b041166f31b5d730a345b3cb18
CaioProg <caioprog18@gmail.com>
2022-01-20 18:54:33 -0300

    Datas

--- JavaScript - Basico/script.js
+++ JavaScript - Basico/script.js
@@ -1,37 +1,3 @@
-function criarAluno(nome, n1, n2){
-  return {
-      nome: nome,
-      nota1: n1,
-      nota2: n2,
-      media: function(){
-        return(this.nota1 + this.nota2) /2;
-      }
-  }
+var d = new Date();
 
-}
-
-function aluno(nome, n1, n2){
-
-  this.nome = nome;
-  this.nota1 = n1;
-  this.nota2 = n2;
-  
-  this.media = function media(){
-    return (this.nota1 + this.nota2) / 2;
-  }
-}
-
-var a = new aluno("caio", 8, 7)
-console.log(a)
-var turma = [
-  criarAluno("Caio", 9, 5),
-  criarAluno("Joao", 9, 5),
-  criarAluno("Maria", 8, 5)
-]
-
-var aluno = turma[0];
-
-turma.forEach( function(elemento){
-
-  console.log(elemento)
-} )
+console.log(d.getSeconds());

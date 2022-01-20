65fd334ce0d796c4941dc5d1b1c593c395d91aea
CaioProg <caioprog18@gmail.com>
2022-01-20 19:00:38 -0300

    Const, Let e Var

--- JavaScript - Basico/script.js
+++ JavaScript - Basico/script.js
@@ -1,3 +1,9 @@
-var d = new Date();
+var numero1 = 4;
+let numero2 = 5;
+const numero3 = [6];
 
-console.log(d.getSeconds());
+numero3.push(7);
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);

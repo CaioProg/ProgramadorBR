789f377e3803ee619ea2b304e161e02fe85d9e47
CaioProg <caioprog18@gmail.com>
2022-01-23 11:34:10 -0300

    JSON

--- JavaScript - Avançado/script.js
+++ JavaScript - Avançado/script.js
@@ -1,2 +1,19 @@
-let pi = Math.round ( Math.random() * 10);
-console.log(pi)
+let a = {
+    nome: "Caio",
+    nota: "5.5"
+}
+let b = JSON.stringify(a);
+
+let a2 = '{"nome": "Maria", "nota":7.8}';
+
+let b2 = JSON.parse(a2);
+
+console.log(a);
+console.log(b);
+
+
+console.log(a2);
+console.log(b2);
+
+console.log(b2.nota);
+console.log(b2.nome);

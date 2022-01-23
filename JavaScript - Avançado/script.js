841ddc539489ed0fb98cd92a66b317512ff856ec
CaioProg <caioprog18@gmail.com>
2022-01-23 11:27:16 -0300

    Biblioteca Math

--- JavaScript - Avançado/script.js
+++ JavaScript - Avançado/script.js
@@ -1,15 +1,2 @@
-
-function loaded() {
-    let t = document.getElementById("titulo");
-   // t.onclick = mudarText;
-   t.addEventListener("mouseover", mudarText)
-   t.addEventListener("mouseout", mouseout)
-}
-
-function mudarText() {
-    this.innerHTML = "NOVO TEXTO";
-}
-
-function mouseout() {
-    this.innerHTML = "Mouse fora do elemento";
-}
+let pi = Math.round ( Math.random() * 10);
+console.log(pi)

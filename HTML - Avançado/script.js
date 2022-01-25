dccd4e793f15d600a6e8fbe21e3c3f232af2f527
CaioProg <caioprog18@gmail.com>
2022-01-25 16:23:17 -0300

    Atributos personalizados - Parte 1

---
+++ HTML - Avançado/script.js
@@ -0,0 +1,13 @@
+let lista = document.getElementById("lista");
+
+let num = parseInt(lista.getAttribute("num"));
+
+let conteudo = "";
+
+for (let i = 0; i < num; i++) {
+
+    conteudo += "<li>" + i + "</li>";
+
+}
+
+lista.innerHTML = conteudo;
